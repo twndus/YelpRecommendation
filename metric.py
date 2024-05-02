ce4a4c55f7df003677c8800d49d5c95f5f5908ee
GangBean <xbox2537@gmail.com>
2024-05-02 11:13:09 +0000

    feat: implements map at k and test code #4

--- metric.py
+++ metric.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from loguru import logger
 
+
 def precision_at_k(actual: np.ndarray, predicted: np.ndarray, k: int=20) -> float:
     '''
     description: calculate mean precision@K of all users
@@ -26,15 +27,15 @@ def precision_at_k(actual: np.ndarray, predicted: np.ndarray, k: int=20) -> floa
 
     return precision_sum / num_users
 
-def recall_at_k(actual: np.ndarray, predicted: np.ndarray, k: int=20):
+def recall_at_k(actual: np.ndarray, predicted: np.ndarray, k: int=20) -> float:
     '''
     description: calculate mean recall@K of all users
-        recall@K = (TP@K / TP + FP)
+        recall@K = (TP@K / num_positive)
     input:
         actual.shape = (num_users, num_items)
         predicted.shape = (num_users, num_items)
     output:
-        mean recall@K or all users: float
+        mean recall@K of all users: float
     '''
     logger.info(f"Calculate Recall@K...")
     recall_sum: float = .0
@@ -50,14 +51,55 @@ def recall_at_k(actual: np.ndarray, predicted: np.ndarray, k: int=20):
 
     return recall_sum / num_users
 
-def ndcg_ag_k():
+def map_at_k(actual: np.ndarray, predicted: np.ndarray, k: int=20) -> float:
+    '''
+    description: calculate mean AP@K of all users
+        AP@K = (Precision@1 * rel_1 + Precision@2 * rel_2 + ... + Precision@K * rel_k) / (num_positive)
+    input:
+        actual.shape = (num_users, num_items)
+        predicted.shape = (num_users, num_items)
+    output:
+        mean AP@K of all users: float
+    '''
+    logger.info(f"Calculate MAP@K...")
+    ap_k_sum: float = .0
+    num_users: int = len(actual)
+
+    for user_idx in range(num_users):
+        user_actual: np.ndarray = actual[user_idx]
+        user_predicted: np.ndarray = predicted[user_idx]
+
+        ap_k: float = _average_precision_at_k(user_actual, user_predicted, k)
+        
+        ap_k_sum += ap_k
+
+    return ap_k_sum / num_users
+
+def _average_precision_at_k(user_actual: np.ndarray, user_predicted: np.ndarray, k:int) -> float:
+    precision_sum: float= .0
+    for i in range(1, k+1):
+        actual_set: set = set(user_actual[:i])
+        predicted_set: set = set(user_predicted[:i])
+
+        precision_at_i: float = len(actual_set & predicted_set) / i
+        rel_i: int = 1 if user_actual[i-1] == user_predicted[i-1] else 0
+
+        precision_sum += (precision_at_i * rel_i)
+    
+    return precision_sum / len(user_actual)
+
+def ndcg_ag_k(actual: np.ndarray, predicted: np.ndarray, k: int=20) -> float:
     '''
     description: calculate mean ndcg@K of all users
-        NDCG@K = ()
+        NDCG@K = ( / K)
+    input:
+        actual.shape = (num_users, num_items)
+        predicted.shape = (num_users, num_items)
+    output:
+        mean NDCG@K or all users: float
     '''
     logger.info(f"Calculate NDCG@K...")
-    pass
+    ndcg_sum: float = .0
+    num_users: int = len(actual)
 
-def map_at_k():
-    logger.info(f"Calculate MAP@K...")
-    pass
+    return ndcg_sum / num_users
--- test/test_metric.py
+++ test/test_metric.py
@@ -2,7 +2,7 @@ import numpy as np
 from math import isclose
 
 from ..metric import (
-    precision_at_k, recall_at_k
+    precision_at_k, recall_at_k, map_at_k
 )
 
 
@@ -16,7 +16,6 @@ def test_precision_at_k():
     assert isclose(precision_at_k(actual, predicted, 4), 0.375)
     assert isclose(precision_at_k(actual, predicted, 5), 0.3)
 
-
 def test_recall_at_k():
     actual: np.ndarray = np.array([[1,2,3,4,5], [6,7,8,9,10]])
     predicted: np.ndarray = np.array([[1,6,7,11,12], [6,7,14,16,20]])
@@ -26,3 +25,13 @@ def test_recall_at_k():
     assert isclose(recall_at_k(actual, predicted, 3), 0.3)
     assert isclose(recall_at_k(actual, predicted, 4), 0.3)
     assert isclose(recall_at_k(actual, predicted, 5), 0.3)
+
+def test_map_at_k():
+    actual: np.ndarray = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+    predicted: np.ndarray = np.array([[1,6,7,11,12], [6,7,14,16,20]])
+
+    assert isclose(map_at_k(actual, predicted, 1), 0.2) # (1/5 + 1/5) / 2
+    assert isclose(map_at_k(actual, predicted, 2), 0.3) # (1/5 + 2/5) / 2
+    assert isclose(map_at_k(actual, predicted, 3), 0.3) # (1/5 + 2/5) / 2
+    assert isclose(map_at_k(actual, predicted, 4), 0.3) # (1/5 + 2/5) / 2
+    assert isclose(map_at_k(actual, predicted, 5), 0.3) # (1/5 + 2/5) / 2

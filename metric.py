56e48e5990773056fe2ba975252a224ff26bd3b2
GangBean <xbox2537@gmail.com>
2024-05-02 10:02:42 +0000

    feat: implements recall_at_k and test code #4

--- metric.py
+++ metric.py
@@ -4,11 +4,13 @@ from loguru import logger
 
 def precision_at_k(actual: np.ndarray, predicted: np.ndarray, k: int=20) -> float:
     '''
+    description: calculate mean precision@K of all users
+        precision@K = (TP@K / K)
     input:
         actual.shape = (num_users, num_items)
         predicted.shape = (num_users, num_items)
     output:
-        mean precision at k of all users: float
+        mean precision@K of all users: float
     '''
     logger.info(f"Calculate precision at k...")
     precision_sum: float = .0
@@ -17,21 +19,45 @@ def precision_at_k(actual: np.ndarray, predicted: np.ndarray, k: int=20) -> floa
     for user_idx in range(num_users):
         actual_set: set = set(actual[user_idx]) # user's total items
         predicted_set: set = set(predicted[user_idx][:k]) # user's predicted top-k items
-        correct_count: int = len(actual_set & predicted_set)
-        user_precision: float = correct_count / k
+        true_positive_count: int = len(actual_set & predicted_set)
+        user_precision: float = true_positive_count / k
 
         precision_sum += user_precision
 
     return precision_sum / num_users
 
-def recall_at_k():
-    logger.info(f"Calculate precision at k...")
-    pass
+def recall_at_k(actual: np.ndarray, predicted: np.ndarray, k: int=20):
+    '''
+    description: calculate mean recall@K of all users
+        recall@K = (TP@K / TP + FP)
+    input:
+        actual.shape = (num_users, num_items)
+        predicted.shape = (num_users, num_items)
+    output:
+        mean recall@K or all users: float
+    '''
+    logger.info(f"Calculate Recall@K...")
+    recall_sum: float = .0
+    num_users: int = len(actual)
+
+    for user_idx in range(num_users):
+        actual_set: set = set(actual[user_idx])
+        predicted_set: set = set(predicted[user_idx][:k])
+        true_positive_count: int = len(actual_set & predicted_set)
+        user_recall: float = true_positive_count / len(actual_set)
+
+        recall_sum += user_recall
+
+    return recall_sum / num_users
 
 def ndcg_ag_k():
-    logger.info(f"Calculate precision at k...")
+    '''
+    description: calculate mean ndcg@K of all users
+        NDCG@K = ()
+    '''
+    logger.info(f"Calculate NDCG@K...")
     pass
 
 def map_at_k():
-    logger.info(f"Calculate precision at k...")
+    logger.info(f"Calculate MAP@K...")
     pass
--- test/test_metric.py
+++ test/test_metric.py
@@ -1,11 +1,28 @@
 import numpy as np
 from math import isclose
 
-from ..metric import precision_at_k
+from ..metric import (
+    precision_at_k, recall_at_k
+)
+
 
 def test_precision_at_k():
     actual: np.ndarray = np.array([[1,2,3,4,5], [6,7,8,9,10]])
     predicted: np.ndarray = np.array([[1,6,7,11,12], [6,7,14,16,20]])
 
+    assert isclose(precision_at_k(actual, predicted, 1), 1)
     assert isclose(precision_at_k(actual, predicted, 2), 0.75)
+    assert isclose(precision_at_k(actual, predicted, 3), 0.5)
+    assert isclose(precision_at_k(actual, predicted, 4), 0.375)
     assert isclose(precision_at_k(actual, predicted, 5), 0.3)
+
+
+def test_recall_at_k():
+    actual: np.ndarray = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+    predicted: np.ndarray = np.array([[1,6,7,11,12], [6,7,14,16,20]])
+
+    assert isclose(recall_at_k(actual, predicted, 1), 0.2)
+    assert isclose(recall_at_k(actual, predicted, 2), 0.3)
+    assert isclose(recall_at_k(actual, predicted, 3), 0.3)
+    assert isclose(recall_at_k(actual, predicted, 4), 0.3)
+    assert isclose(recall_at_k(actual, predicted, 5), 0.3)
